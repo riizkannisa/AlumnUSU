4b6f6b25a833248ee2dd6f89c5838308923055bd
irsyadfauzi <66856780+irsyadfauzi@users.noreply.github.com>
2022-12-28 23:40:43 +0700

    bagian kak fad yang ku push ini, banyak

--- app/src/main/java/com/example/alumnusu/MainActivity.kt
+++ app/src/main/java/com/example/alumnusu/MainActivity.kt
@@ -2,51 +2,22 @@ package com.example.alumnusu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
-import android.widget.PopupMenu
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.lang.Exception
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bottomNav : BottomNavigationView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadFragment(HomeFragment())
-        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> {
-                    loadFragment(HomeFragment())
-                    true
-                }
-                R.id.tracer -> {
-                    loadFragment(TracerFragment())
-                    true
-                }
-                R.id.add -> {
-                    loadFragment(AddFragment())
-                    true
-                }
-                R.id.message -> {
-                    loadFragment(MessageFragment())
-                    true
-                }
-                R.id.profil -> {
-                    loadFragment(ProfilFragment())
-                    true
-                }
-            }
-        }
     }
-    private  fun loadFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container,fragment)
-        transaction.commit()
+
+    fun login() {
+        val i = Intent(applicationContext, activity_login::class.java)
+        startActivity(i)
+    }
+
+    fun register() {
+        val i = Intent(applicationContext, register::class.java)
+        startActivity(i)
     }
 
 }
---
+++ app/src/main/java/com/example/alumnusu/feeds.kt
@@ -0,0 +1,48 @@
+package com.example.alumnusu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class feeds : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feeds)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.tracer -> {
+                    loadFragment(TracerFragment())
+                    true
+                }
+                R.id.add -> {
+                    loadFragment(AddFragment())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(MessageFragment())
+                    true
+                }
+                R.id.profil -> {
+                    loadFragment(ProfilFragment())
+                    true
+                }
+                else -> {false}
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
+}
